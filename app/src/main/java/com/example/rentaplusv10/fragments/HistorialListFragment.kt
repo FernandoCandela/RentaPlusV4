ff1bb7390ba8f770d0cf091e685c82bfb56cedba
FernandoCandela <20171937@aloe.ulima.edu.pe>
2021-12-10 07:51:56 -0500

    add fragment historial

---
+++ app/src/main/java/com/example/rentaplusv10/fragments/HistorialListFragment.kt
@@ -0,0 +1,44 @@
+package com.example.rentaplusv10.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rentaplusv10.R
+import com.example.rentaplusv10.adapter.HistorialListAdapter
+import com.example.rentaplusv10.model.Historial
+import com.example.rentaplusv10.model.InmuebleManager
+
+class HistorialListFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_historial_list,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var id = ""
+        InmuebleManager().getHistorialByInmueble(id,{ vgList : MutableList<Historial> ->
+            val rviPokemon = view.findViewById<RecyclerView>(R.id.rviHistorial)
+            rviPokemon.adapter = HistorialListAdapter(
+                vgList,
+                this
+            ){
+
+            }
+        },{error ->
+            Toast.makeText(activity, "Error: " + error, Toast.LENGTH_SHORT).show()
+        })
+
+    }
+
+
+
+}
